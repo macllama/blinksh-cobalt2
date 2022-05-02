77bbfc4dd75b40392c8796a23996c7163becfa6e
The Couch Llama <llama@couchllama.com>
2022-05-02 15:22:26 -0500

    Lighten foreground color

--- cobalt2.js
+++ cobalt2.js
@@ -1,4 +1,21 @@
-t.prefs_.set('color-palette-overrides',["#000000", "#ff2600", "#3ad900", "#ffc600", "#1478db", "#ff2c70", "#00c5c7", "#c7c7c7", "#808080", "#ff0000", "#33ff00", "#ffff00", "#1478DB", "#cc00ff", "#00ffff", "#FFFFFF"]);
-t.prefs_.set('foreground-color', "#c7c7c7");
-t.prefs_.set('background-color', "#193549");
-t.prefs_.set('cursor-color', '#ffc600');
+t.prefs_.set("color-palette-overrides", [
+  "#000000",
+  "#ff2600",
+  "#3ad900",
+  "#ffc600",
+  "#1478db",
+  "#ff2c70",
+  "#00c5c7",
+  "#c7c7c7",
+  "#808080",
+  "#ff0000",
+  "#33ff00",
+  "#ffff00",
+  "#1478DB",
+  "#cc00ff",
+  "#00ffff",
+  "#FFFFFF",
+]);
+t.prefs_.set("foreground-color", "#e7e7e7");
+t.prefs_.set("background-color", "#193549");
+t.prefs_.set("cursor-color", "#ffc600");
